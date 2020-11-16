466f12d7ee0a396c560f6dc8a74770504f45eed3
XiaoKang <dreamytzk@outlook.com>
2020-11-16 15:25:20 +0800

    ✨ 表单数据获取

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, Input, Button, Checkbox } from 'antd'
+import { Form, Input, Button } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 
 import './login.less'
@@ -7,9 +7,19 @@ import logo from './images/logo.png'
 /**
  * 登录的路由组件
  */
+
 export default class Login extends Component {
+  // 创建表单实例
+  formRef = React.createRef()
   // 表单提交事件
-  handleSubmit = (event) => {}
+  handleSubmit = () => {
+    console.log(this.formRef.current)
+    // 调用 getFieldValue 方法获取Form.Item.name 的值 （即username和password）
+    const { getFieldValue } = this.formRef.current
+    const { username, password } = getFieldValue()
+    console.log(username, password)
+  }
+
   render() {
     return (
       <div className='login'>
@@ -22,9 +32,17 @@ export default class Login extends Component {
           <Form
             name='normal_login'
             className='login-form'
-            onSubmit={this.handleSubmit}
+            // 为表单添加引用
+            ref={this.formRef}
+            // 为表单添加校检完成的事件
+            onFinish={this.handleSubmit}
           >
-            <Form.Item>
+            <Form.Item
+              name='username'
+              rules={[
+                { required: true, message: 'Please input your username!' }
+              ]}
+            >
               <Input
                 prefix={
                   <UserOutlined
@@ -35,7 +53,12 @@ export default class Login extends Component {
                 placeholder='Username'
               />
             </Form.Item>
-            <Form.Item>
+            <Form.Item
+              name='password'
+              rules={[
+                { required: true, message: 'Please input your password!' }
+              ]}
+            >
               <Input
                 prefix={
                   <LockOutlined
