9eb4a77e7af708356ea03fece689c4f784a91635
XiaoKang <dreamytzk@outlook.com>
2020-11-17 10:31:50 +0800

    ✨ bar导航栏自动生成

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -1,15 +1,48 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { createFromIconfontCN } from '@ant-design/icons'
 
 import { Menu } from 'antd'
 import { PieChartOutlined, MailOutlined } from '@ant-design/icons'
 
 import './index.less'
 import logo from '../../assets/images/logo.png'
+import menuList from '../../config/menuConfig.js'
 
+// 子集菜单
 const { SubMenu } = Menu
+// 阿里iconfont
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2208178_anairh40jgh.js'
+})
 // 左侧导航组件
 export default class LeftNav extends Component {
+  // 根据menu的数据数组生成对应的标签数组
+
+  getMenuNodes = (menuList) => {
+    return menuList.map((item) => {
+      if (!item.children) {
+        return (
+          <Menu.Item key={item.key}>
+            <IconFont type={item.icon} />
+            <Link to={item.key}>{item.title}</Link>
+          </Menu.Item>
+        )
+      } else {
+        return (
+          <SubMenu
+            key={item.key}
+            title={item.title}
+            icon={<IconFont type={item.icon} />}
+          >
+            {/* 递归调用 */}
+            {this.getMenuNodes(item.children)}
+          </SubMenu>
+        )
+      }
+    })
+  }
+
   render() {
     return (
       <div className='left-nav'>
@@ -18,24 +51,25 @@ export default class LeftNav extends Component {
           <h1>谷粒后台</h1>
         </Link>
         <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
+          defaultSelectedKeys={['/home']}
+          defaultOpenKeys={['/home']}
           mode='inline'
           theme='dark'
-          // inlineCollapsed={this.state.collapsed}
         >
-          <Menu.Item key='1' icon={<PieChartOutlined />}>
-            首页
+          {this.getMenuNodes(menuList)}
+          {/* <Menu.Item key='/home'>
+            <IconFont type='icon-tuichu' />
+            <Link to='/home'>首页</Link>
           </Menu.Item>
 
-          <SubMenu key='sub1' icon={<MailOutlined />} title='商品'>
-            <Menu.Item key='5' icon={<MailOutlined />}>
-              品类管理
+          <SubMenu key='sub1' title='商品'>
+            <Menu.Item key='/category' icon={<MailOutlined />}>
+              <Link to='/category'>品类管理</Link>
             </Menu.Item>
-            <Menu.Item key='6' icon={<MailOutlined />}>
-              商品管理
+            <Menu.Item key='/product' icon={<MailOutlined />}>
+              <Link to='/product'>商品管理</Link>
             </Menu.Item>
-          </SubMenu>
+          </SubMenu> */}
         </Menu>
       </div>
     )
---
+++ src/config/menuConfig.js
@@ -0,0 +1,57 @@
+const menuList = [
+  {
+    title: '首页', // 菜单标题名称
+    key: '/home', // 对应的path
+    icon: 'icon-index-0-copy', // 图标名称
+  },
+  {
+    title: '商品',
+    key: '/products',
+    icon: 'icon-shangpin',
+    children: [ // 子菜单列表
+      {
+        title: '品类管理',
+        key: '/category',
+        icon: 'icon-leimupinleifenleileibie'
+      },
+      {
+        title: '商品管理',
+        key: '/product',
+        icon: 'icon-guanli'
+      },
+    ]
+  },
+  {
+    title: '用户管理',
+    key: '/user',
+    icon: 'icon-yonghu'
+  },
+  {
+    title: '角色管理',
+    key: '/role',
+    icon: 'icon-jiaose',
+  },
+  {
+    title: '图形图表',
+    key: '/charts',
+    icon: 'icon-biaoge',
+    children: [
+      {
+        title: '柱形图',
+        key: '/charts/bar',
+        icon: 'icon-tubiaozhuxingtu'
+      },
+      {
+        title: '折线图',
+        key: '/charts/line',
+        icon: 'icon-tubiaoxian_tongyong_tongji'
+      },
+      {
+        title: '饼图',
+        key: '/charts/pie',
+        icon: 'icon-tubiao08'
+      },
+    ]
+  },
+]
+export default menuList
