f66744652720fd2e0bdfc428e149c77fd28a0553
XiaoKang <dreamytzk@outlook.com>
2020-11-17 11:31:34 +0800

    ✨ reduce加递归调用生成导航栏 ✨ 自动展开菜单栏

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { createFromIconfontCN } from '@ant-design/icons'
-
+import { withRouter } from 'react-router-dom'
 import { Menu } from 'antd'
-import { PieChartOutlined, MailOutlined } from '@ant-design/icons'
 
 import './index.less'
 import logo from '../../assets/images/logo.png'
@@ -16,10 +15,14 @@ const IconFont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_2208178_anairh40jgh.js'
 })
 // 左侧导航组件
-export default class LeftNav extends Component {
-  // 根据menu的数据数组生成对应的标签数组
 
-  getMenuNodes = (menuList) => {
+class LeftNav extends Component {
+  constructor(props) {
+    super(props)
+    this.menuNodes = this.getMenuNodes(menuList)
+  }
+  // 根据menu的数据数组生成对应的标签数组
+  getMenuNodes_map = (menuList) => {
     return menuList.map((item) => {
       if (!item.children) {
         return (
@@ -42,8 +45,43 @@ export default class LeftNav extends Component {
       }
     })
   }
+  getMenuNodes = (menuList) => {
+    const path = this.props.location.pathname
+    return menuList.reduce((pre, item) => {
+      // 向pre中添加
+      if (!item.children) {
+        pre.push(
+          <Menu.Item key={item.key}>
+            <IconFont type={item.icon} />
+            <Link to={item.key}>{item.title}</Link>
+          </Menu.Item>
+        )
+      } else {
+        const cItem = item.children.find((cItem) => cItem.key === path)
+        // 如果存在
+        if (cItem) this.openKey = item.key
 
+        pre.push(
+          <SubMenu
+            key={item.key}
+            title={item.title}
+            icon={<IconFont type={item.icon} />}
+          >
+            {/* 递归调用 */}
+            {this.getMenuNodes(item.children)}
+          </SubMenu>
+        )
+      }
+      return pre
+    }, [])
+  }
+  com
   render() {
+    // const menuNodes = this.getMenuNodes(menuList)
+    const path = this.props.location.pathname
+    // 需要打开菜单的key
+    const openKey = this.openKey
+
     return (
       <div className='left-nav'>
         <Link className='left-nav-header' to='/home'>
@@ -51,27 +89,16 @@ export default class LeftNav extends Component {
           <h1>谷粒后台</h1>
         </Link>
         <Menu
-          defaultSelectedKeys={['/home']}
-          defaultOpenKeys={['/home']}
+          selectedKeys={[path]}
+          defaultOpenKeys={[openKey]}
           mode='inline'
           theme='dark'
         >
-          {this.getMenuNodes(menuList)}
-          {/* <Menu.Item key='/home'>
-            <IconFont type='icon-tuichu' />
-            <Link to='/home'>首页</Link>
-          </Menu.Item>
-
-          <SubMenu key='sub1' title='商品'>
-            <Menu.Item key='/category' icon={<MailOutlined />}>
-              <Link to='/category'>品类管理</Link>
-            </Menu.Item>
-            <Menu.Item key='/product' icon={<MailOutlined />}>
-              <Link to='/product'>商品管理</Link>
-            </Menu.Item>
-          </SubMenu> */}
+          {this.menuNodes}
         </Menu>
       </div>
     )
   }
 }
+// 通过使用withRouter包装获取路由
+export default withRouter(LeftNav)
