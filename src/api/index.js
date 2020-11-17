b00abf24d1016e41437689f8839b73e72bb60876
XiaoKang <dreamytzk@outlook.com>
2020-11-17 14:56:09 +0800

    ✨ 动态显示天气时间 💄 首页布局

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,35 @@
  * 每个函数的返回值都是Promise
  */
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import { message } from 'antd'
 // 登录
 export const reqLogin = (username, password) => ajax('/login', { username, password }, "POST")
 // 添加用户
 export const reqAddUser = (user) => ajax('/manage/user/add', user, "POST")
+
+// json请求的接口
+
+/*
+json请求的接口请求函数
+ */
+export const reqWeather = (city) => {
+
+  return new Promise((resolve, reject) => {
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    // 发送jsonp请求
+    jsonp(url, {}, (err, data) => {
+      console.log('jsonp()', err, data)
+      // 如果成功了
+      if (!err && data.status === 'success') {
+        // 取出需要的数据
+        const { dayPictureUrl, weather } = data.results[0].weather_data[0]
+        resolve({ dayPictureUrl, weather })
+      } else {
+        // 如果失败了
+        message.error('获取天气信息失败!')
+      }
+
+    })
+  })
+}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
-
+import LinkButton from '../link-button'
 import './index.less'
-
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils.js'
+import { reqWeather } from '../../api/index'
 // 顶部组件
 export default class Header extends Component {
+  state = {
+    currentTime: formateDate(Date.now()),
+    dayPictureUrl: '', // 天气的图片
+    weather: '' // 天气的文本
+  }
+  getTime = () => {
+    setInterval(() => {
+      const currentTime = formateDate(Date.now())
+      this.setState({ currentTime })
+    }, 1000)
+  }
+  getWeather = async () => {
+    const { dayPictureUrl, weather } = await reqWeather('保定')
+    this.setState({ dayPictureUrl, weather })
+  }
+  componentDidMount() {
+    // 第一次render之后执行
+    // 一般在此执行异步操作
+    this.getTime()
+    // 获取当前天气显示
+    this.getWeather()
+  }
   render() {
-    return <div className='header'>Header</div>
+    const { currentTime, dayPictureUrl, weather } = this.state
+    const username = memoryUtils.user.username
+    return (
+      <div className='header'>
+        <div className='header-top'>
+          <span>欢迎, {username}</span>
+          <LinkButton onClick={this.logout}>退出</LinkButton>
+        </div>
+        <div className='header-bottom'>
+          <div className='header-bottom-left'>首页</div>
+          <div className='header-bottom-right'>
+            <span>{currentTime}</span>
+            <img src={dayPictureUrl} alt='weather' />
+            <span>{weather}</span>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
