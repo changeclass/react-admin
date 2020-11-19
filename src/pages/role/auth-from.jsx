1c4d2d7427b0fcaf354b1b08d918d3b5e6918f52
XiaoKang <dreamytzk@outlook.com>
2020-11-19 21:03:07 +0800

    🐛 修复权限无法选中的bug

--- src/pages/role/auth-from.jsx
+++ src/pages/role/auth-from.jsx
@@ -1,84 +1,56 @@
 import React, { Component } from 'react'
-import { Input, Tree } from 'antd'
-import menuList from '../../config/menuConfig'
-import PropType from 'prop-types'
+import { Form, Input, Tree } from 'antd'
+import PropTypes from 'prop-types'
+import menuConfig from '../../config/menuConfig'
 
-export default class AuthForm extends Component {
-  propType = {
-    role: PropType.object
+class AuthForm extends Component {
+  static propTypes = {
+    role: PropTypes.object.isRequired
   }
-  state = {
-    checkedKeys: [],
-    edit: false
-  }
-  formRef = React.createRef()
-  getTreeNodes = (menuList) => {
-    return [{ title: '平台', key: 'all', children: menuList }]
-  }
+
   constructor(props) {
     super(props)
-    const { menus } = this.props.role
     this.state = {
-      checkedKeys: menus
+      treeData: menuConfig,
+      checkedKeys: [],
+      role: {}
     }
-    this.treeNodes = this.getTreeNodes(menuList)
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const menus = nextProps.role.menus
-    this.setState({
-      checkedKeys: menus
-    })
-  }
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   console.log(prevState)
-  //   //该方法内禁止访问this
-  //   if (nextProps.role.menus !== prevState.checkedKeys) {
-  //     //通过对比nextProps和prevState，返回一个用于更新状态的对象
-  //     return {
-  //       checkedKeys: nextProps.role.menus
-  //     }
-  //   }
-  //   //不需要更新状态，返回null
-  //   return null
-  // }
-
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   console.log('nextProps', nextProps.role.menus)
-  //   console.log('prevState', prevState.checkedKeys)
-  //   if (nextProps.role.menus !== prevState.checkedKeys) {
-  //     //通过对比nextProps和prevState，返回一个用于更新状态的对象
-  //     // nextProps.role.menus = prevState.checkedKeys
-  //     return {
-  //       checkedKeys: nextProps.role.menus
-  //     }
-  //   }
-  // }
-  // shouldComponentUpdate(nextProps, prevState) {
-  //   console.log(nextProps, prevState)
-  //   return nextProps.role.menus !== prevState.checkedKeys
-  // }
   onCheck = (checkedKeys) => {
-    this.setState({
-      checkedKeys
-    })
+    this.setState({ checkedKeys })
   }
+
   getMenus = () => this.state.checkedKeys
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { role } = nextProps
+    if (role._id !== prevState.role._id) {
+      prevState.checkedKeys = role.menus
+      prevState.role = role
+    }
+  }
+
   render() {
     const { role } = this.props
-    const { checkedKeys } = this.state
     return (
-      <div>
-        <Input addonBefore='角色名称' disabled value={role.name} />
-
+      <>
+        <Form>
+          <Form.Item label='角色名称'>
+            <Input value={role.name} readOnly></Input>
+          </Form.Item>
+        </Form>
         <Tree
           checkable
-          defaultExpandAll={true}
-          checkedKeys={checkedKeys}
+          defaultExpandAll
+          checkedKeys={this.state.checkedKeys}
+          selectable={false}
           onCheck={this.onCheck}
-          treeData={this.treeNodes}
+          treeData={this.state.treeData}
         />
-      </div>
+      </>
     )
   }
 }
+
+export default AuthForm
