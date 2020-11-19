3c58909f85510b649eb88f8138cf85c8460191c7
XiaoKang <dreamytzk@outlook.com>
2020-11-19 10:27:28 +0800

    📦 安装富文本编辑器插件 ✅ 添加富文本编辑器测试代码

---
+++ src/pages/product/rich-text-editor.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { EditorState, convertToRaw } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg'
+import draftToHtml from 'draftjs-to-html'
+import htmlToDraft from 'html-to-draftjs'
+
+export default class RichTextEditor extends Component {
+  state = {
+    editorState: EditorState.createEmpty()
+  }
+
+  render() {
+    const { editorState } = this.state
+    return (
+      <div>
+        <Editor
+          editorState={editorState}
+          wrapperClassName='demo-wrapper'
+          editorClassName='demo-editor'
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        <textarea
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        />
+      </div>
+    )
+  }
+}
