27c29b1454e85b26ddf0089c3ff725077f005709
XiaoKang <dreamytzk@outlook.com>
2020-11-19 22:43:56 +0800

    🍻 简化了代码 🍻 添加了注释

--- src/pages/role/auth-from.jsx
+++ src/pages/role/auth-from.jsx
@@ -1,40 +1,46 @@
 import React, { Component } from 'react'
-import { Form, Input, Tree } from 'antd'
-import PropTypes from 'prop-types'
-import menuConfig from '../../config/menuConfig'
+import { Input, Tree, Form } from 'antd'
+import menuList from '../../config/menuConfig'
+import PropType from 'prop-types'
 
-class AuthForm extends Component {
-  static propTypes = {
-    role: PropTypes.object.isRequired
+export default class AuthForm extends Component {
+  propType = {
+    role: PropType.object
   }
-
+  formRef = React.createRef()
+  getTreeNodes = (menuList) => {
+    return [{ title: '平台', key: 'all', children: menuList }]
+  }
   constructor(props) {
     super(props)
     this.state = {
-      treeData: menuConfig,
       checkedKeys: [],
-      role: {}
+      // 记录当前角色对象
+      role: []
     }
+    this.treeNodes = this.getTreeNodes(menuList)
+  }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // 判断传入对象与当前记录的对象是否是同一个对象
+    if (nextProps.role !== prevState.role) {
+      return {
+        // 将刚传入的值赋值给已保存的值
+        checkedKeys: nextProps.role.menus,
+        role: nextProps.role
+      }
+    }
+    return null
   }
 
   onCheck = (checkedKeys) => {
     this.setState({ checkedKeys })
   }
-
   getMenus = () => this.state.checkedKeys
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { role } = nextProps
-    if (role._id !== prevState.role._id) {
-      prevState.checkedKeys = role.menus
-      prevState.role = role
-    }
-  }
-
   render() {
     const { role } = this.props
+    const { checkedKeys } = this.state
     return (
-      <>
+      <div>
         <Form>
           <Form.Item label='角色名称'>
             <Input value={role.name} readOnly></Input>
@@ -42,15 +48,12 @@ class AuthForm extends Component {
         </Form>
         <Tree
           checkable
-          defaultExpandAll
-          checkedKeys={this.state.checkedKeys}
-          selectable={false}
+          defaultExpandAll={true}
+          checkedKeys={checkedKeys}
           onCheck={this.onCheck}
-          treeData={this.state.treeData}
+          treeData={this.treeNodes}
         />
-      </>
+      </div>
     )
   }
 }
-
-export default AuthForm
