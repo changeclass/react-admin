9955967c87cbfca0a0c89cde4c4bd44479fd667d
XiaoKang <dreamytzk@outlook.com>
2020-11-21 09:41:36 +0800

    ✨ 显示用户管理页面

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,8 +1,121 @@
 import React, { Component } from 'react'
-
+import { Card, Button, Table, Modal, message } from 'antd'
+import { formateDate } from '../../utils/dateUtils'
+import LinkButton from '../../components/link-button/index'
+import { reqDeleteUser, reqUsers, reqAddOrUpdateUser } from '../../api/index'
+import UserForm from './user-form'
 // 用户路由
 export default class User extends Component {
+  state = {
+    users: [], // 所有用户列表
+    roles: [], // 所有角色列表
+    isShow: false // 是否显示确认框
+  }
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '用户名',
+        dataIndex: 'username'
+      },
+      {
+        title: '邮箱',
+        dataIndex: 'email'
+      },
+
+      {
+        title: '电话',
+        dataIndex: 'phone'
+      },
+      {
+        title: '注册时间',
+        dataIndex: 'create_time',
+        render: formateDate
+      },
+      {
+        title: '所属角色',
+        dataIndex: 'role_id',
+        render: (role_id) => this.roleNames[role_id]
+      },
+      {
+        title: '操作',
+        render: (user) => (
+          <span>
+            <LinkButton onClick={() => this.showUpdate(user)}>修改</LinkButton>
+            <LinkButton onClick={() => this.deleteUser(user)}>删除</LinkButton>
+          </span>
+        )
+      }
+    ]
+  }
+  /*
+  根据role的数组, 生成包含所有角色名的对象(属性名用角色id值)
+   */
+  initRoleNames = (roles) => {
+    const roleNames = roles.reduce((pre, role) => {
+      pre[role._id] = role.name
+      return pre
+    }, {})
+    // 保存
+    this.roleNames = roleNames
+  }
+  getUsers = async () => {
+    const result = await reqUsers()
+    if (result.status === 0) {
+      const { users, roles } = result.data
+      this.initRoleNames(roles)
+      this.setState({
+        users,
+        roles
+      })
+    }
+  }
+  showAdd = () => {
+    this.setState({ isShow: true })
+  }
+  columns = () => {}
+  componentWillMount() {
+    this.initColumns()
+  }
+
+  componentDidMount() {
+    this.getUsers()
+  }
   render() {
-    return <div>User</div>
+    const { users, roles, isShow } = this.state
+    const user = this.user || {}
+
+    const title = (
+      <Button type='primary' onClick={this.showAdd}>
+        创建用户
+      </Button>
+    )
+
+    return (
+      <Card title={title}>
+        <Table
+          bordered
+          rowKey='_id'
+          dataSource={users}
+          columns={this.columns}
+          pagination={{ defaultPageSize: 2 }}
+        />
+
+        <Modal
+          title={user._id ? '修改用户' : '添加用户'}
+          visible={isShow}
+          onOk={this.addOrUpdateUser}
+          onCancel={() => {
+            this.form.resetFields()
+            this.setState({ isShow: false })
+          }}
+        >
+          <UserForm
+            // setForm={(form) => (this.form = form)}
+            roles={roles}
+            user={user}
+          />
+        </Modal>
+      </Card>
+    )
   }
 }
